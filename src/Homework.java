0b6cdb12da7f993ea63aa74faea10eb543a516ff
Nikita Pantsevich <pantsevich15@yandex.ru>
2022-11-16 18:44:50 +0300

    did task2

--- src/Homework.java
+++ src/Homework.java
@@ -5,6 +5,7 @@ public class Homework {
 
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -15,5 +16,30 @@ public class Homework {
         } else {
             System.out.println("This number is odd");
         }
+    }
+
+    public static void task2() {
+        System.out.print("Enter three numbers: ");
+
+        int firstInput = scanner.nextInt();
+        int secondInput = scanner.nextInt();
+        int thirdInput = scanner.nextInt();
+
+        int first = firstInput > 0 ? firstInput : -firstInput;
+        int second = secondInput > 0 ? secondInput : -secondInput;
+        int third = thirdInput > 0 ? thirdInput : -thirdInput;
+
+        int minimum;
+        if (first > third && second > third) {
+            minimum = third;
+        } else if (first > second && third > second) {
+            minimum = second;
+        } else if (second > first && third > first) {
+            minimum = first;
+        } else {
+            minimum = first;
+        }
+
+        System.out.println("Minimum number: " + minimum);
     }
 }
