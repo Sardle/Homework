bf4daef0e526aca6a172ca81d91c28b011ab18bb
Nikita Pantsevich <pantsevich15@yandex.ru>
2022-11-18 17:42:12 +0300

    did task2

--- src/HomeworkTwoArrays.java
+++ src/HomeworkTwoArrays.java
@@ -8,6 +8,7 @@ public class HomeworkTwoArrays {
 
     public static void main(String[] args) {
         twoDimenToOneDimen();
+        onlyText();
     }
 
     public static void twoDimenToOneDimen() {
@@ -37,5 +38,55 @@ public class HomeworkTwoArrays {
             }
         }
         System.out.println("The resulting one-dimensional array: " + Arrays.toString(oneArray));
+    }
+
+    public static void onlyText() {
+        System.out.print("Enter three numbers for the dimension of the array: ");
+        int[][][] threeArray = new int[scanner.nextInt()][scanner.nextInt()][scanner.nextInt()];
+        for (int i = 0; i < threeArray.length; i++) {
+            for (int j = 0; j < threeArray[i].length; j++) {
+                for (int k = 0; k < threeArray[i][j].length; k++) {
+                    threeArray[i][j][k] = random.nextInt(11);
+                }
+            }
+        }
+        System.out.println("The resulting array:");
+        for (int[][] twoArray : threeArray) {
+            for (int[] array : twoArray) {
+                for (int anInt : array) {
+                    System.out.print(anInt + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        System.out.println("Replaced array:");
+        for (int[][] twoArray : threeArray) {
+            for (int[] array : twoArray) {
+                for (int anInt : array) {
+                    System.out.print(numbers(anInt) + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    private static String numbers(int number) {
+        return switch (number) {
+            case 0 -> "zero";
+            case 1 -> "one";
+            case 2 -> "two";
+            case 3 -> "three";
+            case 4 -> "four";
+            case 5 -> "five";
+            case 6 -> "six";
+            case 7 -> "seven";
+            case 8 -> "eight";
+            case 9 -> "nine";
+            case 10 -> "ten";
+            default -> null;
+        };
     }
 }
