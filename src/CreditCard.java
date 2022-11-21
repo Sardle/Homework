24b8e9470edb1fbb807adfd41df4a150bf0fbb0e
Nikita Pantsevich <pantsevich15@yandex.ru>
2022-11-22 01:48:41 +0300

    did task

---
+++ src/CreditCard.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class CreditCard {
+    private String accountNumber;
+    private int accountStatus;
+
+    public CreditCard(String accountNumber, int accountStatus) {
+        this.accountNumber = accountNumber;
+        this.accountStatus = accountStatus;
+    }
+
+    public void addUpAmount(int amount) {
+        accountStatus += amount;
+    }
+
+    public void withdrawAmount(int amount) {
+        accountStatus -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Account number: " + accountNumber + "\nAccount status: $" + accountStatus + "\n";
+    }
+
+    public static void operation() {
+        Scanner scanner = new Scanner(System.in);
+
+        CreditCard cardUserMikita = new CreditCard("BY3847359NX", 5000);
+        CreditCard cardUserOleg = new CreditCard("BY3454564NX", 1000);
+        CreditCard cardUserJulia = new CreditCard("BY23423429NX", 10000);
+
+        System.out.println(cardUserMikita);
+        System.out.println(cardUserOleg);
+        System.out.println(cardUserJulia);
+
+        System.out.print("Enter the amount of thr operation to add: ");
+        cardUserMikita.addUpAmount(scanner.nextInt());
+        System.out.print("Enter the amount of thr operation to add: ");
+        cardUserOleg.addUpAmount(scanner.nextInt());
+        System.out.print("Enter the amount of thr operation to reduce: ");
+        cardUserJulia.withdrawAmount(scanner.nextInt());
+
+        System.out.println("\n" + cardUserMikita);
+        System.out.println(cardUserOleg);
+        System.out.println(cardUserJulia);
+    }
+
+    public static void main(String[] args) {
+        operation();
+    }
+}
