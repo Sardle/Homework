52c76849535e4538493d6e458f61ae0ebaf53755
Nikita Pantsevich <pantsevich15@yandex.ru>
2022-11-29 01:32:24 +0300

    create branch new_tech_bad

--- src/leason8/homework/Designer.java
+++ src/leason8/homework/Designer.java
@@ -1,5 +1,5 @@
 package leason8.homework;
 
 public class Designer {
-    private Workplace workplace = new Workplace("HP", "Logitech","Logitech", "Samsung");
+    private Workplace workplace = new Workplace("MacBook");
 }
--- src/leason8/homework/ProjectManager.java
+++ src/leason8/homework/ProjectManager.java
@@ -1,5 +1,5 @@
 package leason8.homework;
 
 public class ProjectManager {
-    private Workplace workplace = new Workplace("HP", "Logitech","Logitech", "Samsung");
+    private Workplace workplace = new Workplace("MacBook");
 }
