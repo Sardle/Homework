c13846e31e1c8d45f34c7169f1203a8e72d744bc
Sardle <pantsevich15@yandex.ru>
2022-12-03 13:50:34 +0300

    fix

--- src/leason9/homework/App.java
+++ src/leason9/homework/App.java
@@ -3,29 +3,35 @@ package leason9.homework;
 import java.util.Scanner;
 
 public class App {
+    private static final String CHECK_DATA = "[A-Za-z\\d_]{1,20}";
 
     public static void main(String[] args) {
-        checkData();
+        inputCheck();
     }
 
-    public static void checkData() {
+    public static void inputCheck() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите логин: ");
         String login = scanner.nextLine();
         System.out.print("Введите пароль: ");
         String password = scanner.nextLine();
+        checkInputData(login, password);
+    }
+
+    public static void checkInputData(String login, String password) {
         try {
-            if (task(login, password, "Qwerty")) {
+            if (checkLoginAndPassword(login, password, "Qwerty")) {
                 System.out.println("Все верно! Вход разрешен");
             }
         } catch (WrongLoginException | WrongPasswordException e) {
             System.out.println(e.getMessage());
         }
     }
-    public static boolean task(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        if (!login.matches("[A-Za-z\\d_]{1,20}")) {
+
+    private static boolean checkLoginAndPassword(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (!login.matches(CHECK_DATA)) {
             throw new WrongLoginException("Неверный логин");
-        } else if (!password.matches("[A-Za-z\\d_]{1,20}") || !password.equals(confirmPassword)) {
+        } else if (!password.matches(CHECK_DATA) || !password.equals(confirmPassword)) {
             throw new WrongPasswordException("Неверный пароль");
         } else {
             return true;
--- src/leason9/homework/WrongPasswordException.java
+++ src/leason9/homework/WrongPasswordException.java
@@ -1,9 +1,8 @@
 package leason9.homework;
 
-public class WrongPasswordException extends Exception{
+public class WrongPasswordException extends Exception {
 
     public WrongPasswordException() {
-
     }
 
     public WrongPasswordException(String message) {
