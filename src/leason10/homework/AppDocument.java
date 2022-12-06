7427028959e2e8ea1ac1ca4d141b1052388b034d
Sardle <pantsevich15@yandex.ru>
2022-12-06 18:13:36 +0300

    fix

--- src/leason10/homework/AppDocument.java
+++ src/leason10/homework/AppDocument.java
@@ -7,25 +7,13 @@ import java.io.InputStreamReader;
 public class AppDocument {
 
     public static void main(String[] args) {
-        printInfoFromDocument();
-    }
-
-    public static void printInfoFromDocument() {
+        System.out.print("Enter the document number: ");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         try {
-            System.out.println("Document Template: xxxx-yyy-xxxx-yyy-xyxy. " +
-                    "Where \"x\" is a digit, and \"y\" is a letter of the English alphabet.\n");
-            System.out.print("Enter the document number: ");
             String input = bufferedReader.readLine();
-            if (!Document.checkPattern(input)) {
-                System.out.println("You entered the document number incorrectly! Try again!");
-                printInfoFromDocument();
-            } else {
-                Document.printTwoBlocksNumbers(input);
-                Document.printReplaceBlocks(input);
-                Document.printLetters(input);
-                Document.checkAbc(input);
-                Document.checkSequence(input);
+            while (!Document.printInfoDocument(input)) {
+                System.out.print("\nEnter the document number: ");
+                input = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/leason10/homework/Document.java
+++ src/leason10/homework/Document.java
@@ -3,22 +3,24 @@ package leason10.homework;
 public class Document {
     private static final String DOCUMENT_PATTERN = "\\d{4}-[A-z]{3}-\\d{4}-[A-z]{3}-\\d[A-z]\\d[A-z]";
 
-    public static boolean checkPattern(String document) {
+
+    private static boolean checkPattern(String document) {
         return document.matches(DOCUMENT_PATTERN);
     }
 
-    public static void printTwoBlocksNumbers(String document) {
+
+    private static void printTwoBlocksNumbers(String document) {
         String twoBlocksNumbers = document.replaceAll("-...-\\d[A-z]\\d[A-z]", "")
                 .replaceAll("-[A-z]{3}", "");
         System.out.println("\nThe first two blocks of digits: " + twoBlocksNumbers);
     }
 
-    public static void printReplaceBlocks(String document) {
+    private static void printEntcodedNumber(String document) {
         String replaceBlocks = document.replaceAll("[A-z]{3}", "***");
         System.out.println("Replaced document number: " + replaceBlocks);
     }
 
-    public static void printLetters(String document) {
+    private static void printLetters(String document) {
         StringBuilder sb = new StringBuilder(document);
         String withoutNumbers = sb.delete(0, 5)
                 .delete(4, 9)
@@ -32,7 +34,7 @@ public class Document {
         System.out.println("Letters from the document number: " + withoutNumbers);
     }
 
-    public static void checkAbc(String document) {
+    private static void checkContainsAbc(String document) {
         if (document.toLowerCase().matches(".*abc.*")) {
             System.out.println("Yes, the document contains the sequence abc!");
         } else {
@@ -40,11 +42,27 @@ public class Document {
         }
     }
 
-    public static void checkSequence(String document) {
+    private static void checkEndsWith1a2b(String document) {
         if (document.endsWith("1a2b")) {
             System.out.println("Yes, the document contains the sequence 1a2b!");
         } else {
             System.out.println("No, the document does not contain the sequence 1a2b!");
+        }
+    }
+
+    public static boolean printInfoDocument(String input) {
+        System.out.println("Document Template: xxxx-yyy-xxxx-yyy-xyxy. " +
+                "Where \"x\" is a digit, and \"y\" is a letter of the English alphabet.");
+        if (!Document.checkPattern(input)) {
+            System.out.println("You entered the document number incorrectly! Try again!");
+            return false;
+        } else {
+            Document.printTwoBlocksNumbers(input);
+            Document.printEntcodedNumber(input);
+            Document.printLetters(input);
+            Document.checkContainsAbc(input);
+            Document.checkEndsWith1a2b(input);
+            return true;
         }
     }
 }
